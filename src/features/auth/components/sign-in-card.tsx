a04ebb2c975570e49a8a2caa12352d5f2b82d43a
Yash Pratap Singh <singhyashpratap06@gmail.com>
2025-03-21 16:37:47 +0530

    Create UI for sign-in and sign-up pages

--- src/features/auth/components/sign-in-card.tsx
+++ src/features/auth/components/sign-in-card.tsx
@@ -1,19 +1,13 @@
 import { z } from "zod";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
+
 import { FcGoogle } from "react-icons/fc";
 import { FaGithub } from "react-icons/fa";
 import { DottedSeparator } from "@/components/dotted-separator";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import Link from "next/link";
 import {
   Form,
   FormControl,
@@ -21,17 +15,16 @@ import {
   FormItem,
   FormMessage,
 } from "@/components/ui/form";
+import Link from "next/link";
 
 const formSchema = z.object({
-  name: z.string().trim().min(1, "Required"),
   email: z.string().email(),
-  password: z.string().min(8, "Minimum of 8 characters required"),
+  password: z.string().min(1, "Required"),
 });
 
-export const SignUpCard = () => {
-  const form = useForm({
+export const SignInCard = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
     defaultValues: {
-      name: "",
       email: "",
       password: "",
     },
@@ -44,18 +37,8 @@ export const SignUpCard = () => {
 
   return (
     <Card className="w-full h-full md:w-[487px] border-none shadow-none">
-      <CardHeader className="flex flex-col items-center justify-center text-center p-7 ">
-        <CardTitle className="text-2xl">Sign Up</CardTitle>
-        <CardDescription>
-          By signing up, you agree to our{" "}
-          <Link href="/privacy-policy">
-            <span className="text-blue-700">Privacy Policy</span>
-          </Link>{" "}
-          and{" "}
-          <Link href="/terms">
-            <span className="text-blue-700">Terms of Service</span>
-          </Link>
-        </CardDescription>
+      <CardHeader className="flex items-center justify-center text-center p-7">
+        <CardTitle className="text-2xl">Welcome back!</CardTitle>
       </CardHeader>
       <div className="px-7">
         <DottedSeparator />
@@ -67,22 +50,8 @@ export const SignUpCard = () => {
             className="space-y-4"
           >
             <FormField
-              name="name"
-              render={({ field }) => (
-                <FormItem>
-                  <FormControl>
-                    <Input
-                      {...field}
-                      type="text"
-                      placeholder="Enter your name"
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
               name="email"
+              control={form.control}
               render={({ field }) => (
                 <FormItem>
                   <FormControl>
@@ -98,13 +67,14 @@ export const SignUpCard = () => {
             />
             <FormField
               name="password"
+              control={form.control}
               render={({ field }) => (
                 <FormItem>
                   <FormControl>
                     <Input
                       {...field}
                       type="password"
-                      placeholder="Enter your password"
+                      placeholder="Enter email address"
                     />
                   </FormControl>
                   <FormMessage />
@@ -144,9 +114,9 @@ export const SignUpCard = () => {
         <DottedSeparator />
       </div>
       <CardContent className="p07 flex items-center justify-center">
-        <p>Already have an account?</p>
-        <Link href="/sign-in">
-          <span className="text-blue-700">&nbsp;Sign In</span>
+        <p>Don&apos;t have an account?</p>
+        <Link href="/sign-up">
+          <span className="text-blue-700">&nbsp;Sign Up</span>
         </Link>
       </CardContent>
     </Card>
