c7a9005fa8b42e3f1b3986377c8e2ac99baa7542
Yash Pratap Singh <singhyashpratap06@gmail.com>
2025-03-24 10:39:25 +0530

    Setup route guards and onSuccess redirects for login/register pages and mutations

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,7 +1,12 @@
-"use client";
+import { getCurrent } from "@/features/auth/actions";
 import { SignInCard } from "@/features/auth/components/sign-in-card";
+import { redirect } from "next/navigation";
 
-const SignIn = () => {
+const SignIn = async () => {
+  const user = await getCurrent();
+  if (user) {
+    redirect("/");
+  }
   return <SignInCard />;
 };
 
--- src/features/auth/components/sign-up-card.tsx
+++ src/features/auth/components/sign-up-card.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { z } from "zod";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
@@ -109,7 +110,7 @@ export const SignUpCard = () => {
               )}
             />
             <Button disabled={false} size="lg" className="w-full">
-              Login
+              Register
             </Button>
           </form>
         </Form>
