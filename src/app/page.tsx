b9831473fe34e4b99b17055b355fb43ff9d854d9
Yash Pratap Singh <singhyashpratap06@gmail.com>
2025-03-24 10:37:38 +0530

    Create server action for getting current user, setup route guard for /

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,16 @@
-"use client";
+import { getCurrent } from "@/features/auth/actions";
+import { UserButton } from "@/features/auth/components/user-button";
+import { redirect } from "next/navigation";
 
-import { Button } from "@/components/ui/button";
-import { useCurrent } from "@/features/auth/api/use-current";
-import { useLogout } from "@/features/auth/api/use-logout";
-import { useRouter } from "next/navigation";
-import { useEffect } from "react";
-
-export default function Home() {
-  const router = useRouter();
-  const { data, isLoading } = useCurrent();
-  const { mutate } = useLogout();
-
-  useEffect(() => {
-    if (!data && !isLoading) {
-      router.push("/sign-in");
-    }
-  }, [data]);
+export default async function Home() {
+  const user = await getCurrent();
+  if (!user) {
+    redirect("/sign-in");
+  }
 
   return (
-    <div className="flex">
-      <Button onClick={() => mutate()}>Logout</Button>
+    <div>
+      <UserButton />
     </div>
   );
 }
