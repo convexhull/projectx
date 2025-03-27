56836b96955e77f3a356e5bb5892ae1bc6c6d849
Yash Pratap Singh <singhyashpratap06@gmail.com>
2025-03-27 19:11:43 +0530

    Add mutation for updating workspace settings, add getWorkspace() to fetch workspace initial values for edit form

--- src/features/workspaces/actions.ts
+++ src/features/workspaces/actions.ts
@@ -2,6 +2,8 @@ import { cookies } from "next/headers";
 import { AUTH_COOKIE } from "../auth/constants";
 import { Client, Account, Databases, Query } from "node-appwrite";
 import { DATABASE_ID, MEMBERS_ID, WORKSPACES_ID } from "@/config";
+import { getMember } from "../members/utils";
+import { Workspace } from "./types";
 
 // This is not a server action
 export const getWorkspaces = async () => {
@@ -37,5 +39,46 @@ export const getWorkspaces = async () => {
     return workspaces;
   } catch {
     return { documents: [], total: 0 };
+  }
+};
+
+interface GetWorkspaceProps {
+  workspaceId: string;
+}
+
+export const getWorkspace = async ({ workspaceId }: GetWorkspaceProps) => {
+  try {
+    const client = new Client()
+      .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
+      .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT!);
+
+    const session = (await cookies()).get(AUTH_COOKIE);
+
+    if (!session) return null;
+
+    client.setSession(session.value);
+    const databases = new Databases(client);
+    const account = new Account(client);
+    const user = await account.get();
+
+    const member = await getMember({
+      databases,
+      userId: user.$id,
+      workspaceId,
+    });
+
+    if (!member) {
+      return null;
+    }
+
+    const workspace = await databases.getDocument<Workspace>(
+      DATABASE_ID,
+      WORKSPACES_ID,
+      workspaceId
+    );
+
+    return workspace;
+  } catch {
+    return null;
   }
 };
