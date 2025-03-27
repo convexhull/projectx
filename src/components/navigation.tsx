419c75621a3432186cf4b8fe0749a448d4407da8
Yash Pratap Singh <singhyashpratap06@gmail.com>
2025-03-27 19:14:24 +0530

    Add edit workspace form ui, connect to backend, update navigation links

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -1,4 +1,6 @@
+"use client";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { SettingsIcon, UsersIcon } from "lucide-react";
 import {
   GoCheckCircle,
@@ -7,6 +9,7 @@ import {
   GoHomeFill,
 } from "react-icons/go";
 import { cn } from "@/lib/utils";
+import { useWorkspaceId } from "@/features/workspaces/hooks/use-workspace-id";
 
 const routes = [
   {
@@ -36,14 +39,18 @@ const routes = [
 ];
 
 export const Navigation = () => {
+  const workspaceId = useWorkspaceId();
+  const pathname = usePathname();
+
   return (
     <ul className="flex flex-col">
       {routes.map((item) => {
-        const isActive = false;
+        const fullHref = `/workspaces/${workspaceId}${item.href}`;
+        const isActive = pathname === fullHref;
         const Icon = isActive ? item.activeIcon : item.icon;
 
         return (
-          <Link key={item.href} href={item.href}>
+          <Link key={item.href} href={fullHref}>
             <div
               className={cn(
                 "flex items-center gap-2.5 p-2.5 rounded-md font-medium hover:text-primary transition text-neutral-500",
