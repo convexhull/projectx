fb07e31c31acac5039d529345fd2bc9b226a3756
Yash Pratap Singh <singhyashpratap06@gmail.com>
2025-03-29 03:38:43 +0530

    Create api and mutation hook for joining workspace by invite code

---
+++ src/app/(standalone)/workspaces/[workspaceId]/join/[inviteCode]/page.tsx
@@ -0,0 +1,31 @@
+import { getCurrent } from "@/features/auth/queries";
+import { JoinWorkspaceForm } from "@/features/workspaces/components/join-workspace-form";
+import { getWorkspaceInfo } from "@/features/workspaces/queries";
+import { redirect } from "next/navigation";
+
+type WorkspaceIdJoinPageProps = {
+  params: Promise<{ workspaceId: string }>;
+};
+
+const WorkspaceIdJoinPage = async ({ params }: WorkspaceIdJoinPageProps) => {
+  const user = await getCurrent();
+  if (!user) {
+    redirect("/sign-in");
+  }
+  const { workspaceId } = await params;
+  const initialValues = await getWorkspaceInfo({
+    workspaceId,
+  });
+
+  if (!initialValues) {
+    redirect("/");
+  }
+
+  return (
+    <div className="w-full lg:max-w-xl">
+      <JoinWorkspaceForm initialValues={initialValues} />
+    </div>
+  );
+};
+
+export default WorkspaceIdJoinPage;
