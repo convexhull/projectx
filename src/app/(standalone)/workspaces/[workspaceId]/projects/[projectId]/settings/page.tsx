c03796a89afb249fc131fbcb3bce8d87e7f3479a
Yash Pratap Singh <singhyashpratap06@gmail.com>
2025-03-30 22:48:37 +0530

    Create edit-project-form UI to update project settings

---
+++ src/app/(standalone)/workspaces/[workspaceId]/projects/[projectId]/settings/page.tsx
@@ -0,0 +1,27 @@
+import { getCurrent } from "@/features/auth/queries";
+import { EditProjectForm } from "@/features/projects/components/edit-project-form";
+import { getProject } from "@/features/projects/queries";
+import { redirect } from "next/navigation";
+
+type ProjectIdSettingsPageProps = {
+  params: Promise<{ projectId: string }>;
+};
+
+const ProjectIdSettingsPage = async ({
+  params,
+}: ProjectIdSettingsPageProps) => {
+  const user = await getCurrent();
+  if (!user) {
+    redirect("/sign-in");
+  }
+  const { projectId } = await params;
+  const initialValues = await getProject({ projectId });
+
+  return (
+    <div className="w-full lg:max-w-xl">
+      <EditProjectForm initialValues={initialValues} />
+    </div>
+  );
+};
+
+export default ProjectIdSettingsPage;
---
+++ src/features/projects/components/edit-project-form.tsx
@@ -0,0 +1,261 @@
+"use client";
+import { useForm } from "react-hook-form";
+import { useRef } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { updateProjectSchema } from "../schemas";
+import { z } from "zod";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { DottedSeparator } from "@/components/dotted-separator";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
+import { ArrowLeftIcon, ImageIcon } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { cn } from "@/lib/utils";
+import { Project } from "../types";
+import { useUpdateProject } from "../api/use-update-project";
+import { useConfirm } from "@/hooks/use-confirm";
+
+type EditProjectFormProps = {
+  onCancel?: () => void;
+  initialValues: Project;
+};
+
+export const EditProjectForm = ({
+  onCancel,
+  initialValues,
+}: EditProjectFormProps) => {
+  const router = useRouter();
+  const { mutate, isPending } = useUpdateProject();
+  // const { mutate: deleteWorkspace, isPending: isDeletingWorkspace } =
+  //   useDeleteWorkspace();
+
+  const [DeleteDialog, confirmDelete] = useConfirm(
+    "Delete Workspace",
+    "This action cannot be undone",
+    "destructive"
+  );
+
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const form = useForm<z.infer<typeof updateProjectSchema>>({
+    resolver: zodResolver(updateProjectSchema),
+    defaultValues: {
+      ...initialValues,
+      image: initialValues.imageUrl ?? "",
+    },
+  });
+
+  const handleDelete = async () => {
+    const ok = await confirmDelete();
+    if (!ok) return;
+
+    // deleteWorkspace(
+    //   {
+    //     param: { workspaceId: initialValues.$id },
+    //   },
+    //   {
+    //     onSuccess: () => {
+    //       router.push("/");
+    //     },
+    //   }
+    // );
+  };
+
+  const onSubmit = (values: z.infer<typeof updateProjectSchema>) => {
+    const finalValues = {
+      ...values,
+      image: values.image instanceof File ? values.image : "",
+    };
+    mutate(
+      { form: finalValues, param: { projectId: initialValues.$id } },
+      {
+        onSuccess: () => {
+          form.reset();
+        },
+      }
+    );
+  };
+  //Imp: TS type
+  const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    // Imp: Accessing using [] on ?
+    const file = e.target.files?.[0];
+    if (file) {
+      form.setValue("image", file);
+    }
+  };
+
+  return (
+    <div className="flex flex-col gap-y-4">
+      <DeleteDialog />
+      <Card className="w-full h-full border-none shadow-none">
+        <CardHeader className="flex flex-row items-center gap-x-4 space-y-0">
+          <Button
+            size="sm"
+            variant="secondary"
+            onClick={
+              onCancel
+                ? onCancel
+                : () =>
+                    router.push(
+                      `/workspaces/${initialValues.workspaceId}/projects/${initialValues.$id}`
+                    )
+            }
+          >
+            <ArrowLeftIcon className="size-4" />
+            Back
+          </Button>
+          <CardTitle className="text-xl font-bold">
+            {initialValues.name}
+          </CardTitle>
+        </CardHeader>
+        <div>
+          <DottedSeparator />
+        </div>
+        <CardContent>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+              <div className="flex flex-col gap-y-4">
+                <FormField
+                  control={form.control}
+                  name="name"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Project Name</FormLabel>
+                      <FormControl>
+                        <Input {...field} placeholder="Enter project name" />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="image"
+                  render={({ field }) => (
+                    <div className="flex flex-col gap-y-2">
+                      <div className="flex items-center gap-x-5">
+                        {field.value ? (
+                          <div className="size-[72px] relative rounded-md overflow-hidden">
+                            <Image
+                              alt="Logo"
+                              fill
+                              className="object-cover"
+                              src={
+                                field.value instanceof File
+                                  ? //   Imp: createObjectURL
+                                    URL.createObjectURL(field.value)
+                                  : field.value
+                              }
+                            />
+                          </div>
+                        ) : (
+                          <Avatar className="size-[72px]">
+                            <AvatarFallback>
+                              <ImageIcon className="size-[36px] text-neutral-400" />
+                            </AvatarFallback>
+                          </Avatar>
+                        )}
+                        <div className="flex flex-col">
+                          <p className="text-sm">Project Icon</p>
+                          <p className="text-sm text-muted-foreground">
+                            JPG, PNG, SVG or JPEG, max 1mb
+                          </p>
+                          <input
+                            className="hidden"
+                            type="file"
+                            accept=".jpg, .png, .svg, .jpeg"
+                            ref={inputRef}
+                            disabled={isPending}
+                            onChange={handleImageChange}
+                          />
+                          {field.value ? (
+                            <Button
+                              type="button"
+                              disabled={isPending}
+                              variant="destructive"
+                              size="xs"
+                              className="w-fit mt-2"
+                              onClick={() => {
+                                field.onChange(null);
+                                if (inputRef.current) {
+                                  inputRef.current.value = "";
+                                }
+                              }}
+                            >
+                              Remove Image
+                            </Button>
+                          ) : (
+                            <Button
+                              type="button"
+                              disabled={isPending}
+                              variant="tertiary"
+                              size="xs"
+                              className="w-fit mt-2"
+                              onClick={() => inputRef.current?.click()}
+                            >
+                              Upload Image
+                            </Button>
+                          )}
+                        </div>
+                      </div>
+                    </div>
+                  )}
+                />
+              </div>
+              <DottedSeparator className="py-7" />
+              <div className="flex items-center justify-between">
+                <Button
+                  type="button"
+                  size="lg"
+                  variant="secondary"
+                  disabled={isPending}
+                  onClick={onCancel}
+                  className={cn(!onCancel && "invisible")}
+                >
+                  Cancel
+                </Button>
+                <Button type="submit" size="lg" disabled={isPending}>
+                  Save Changes
+                </Button>
+              </div>
+            </form>
+          </Form>
+        </CardContent>
+      </Card>
+      <Card className="w-full h-full border-none shadow-none">
+        <CardContent>
+          <div className="flex flex-col">
+            <h3 className="font-bold">Danger Zone</h3>
+            <p className="text-sm text-muted-foreground">
+              Deleting a project is irreversible and will remove all associated
+              data
+            </p>
+            <DottedSeparator className="py-7" />
+            <Button
+              size="sm"
+              variant="destructive"
+              type="button"
+              disabled={isPending}
+              className="mt-6 w-fit ml-auto"
+              onClick={handleDelete}
+            >
+              Delete Project
+            </Button>
+          </div>
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
+
+//TODO: Input not instance of file when removing image and saving changes. Also in create form. maybe field.onChange('') instead of null in the remove image button
