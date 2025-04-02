18ab6ebb247771132094146b95262218baff0fa1
Yash Pratap Singh <singhyashpratap06@gmail.com>
2025-04-02 08:51:29 +0530

    Fix tasks api using wrong id for members

--- src/features/tasks/server/route.ts
+++ src/features/tasks/server/route.ts
@@ -86,7 +86,7 @@ const app = new Hono()
       );
       const assignees = await Promise.all(
         members.documents.map(async (member) => {
-          const user = await users.get(member.$id);
+          const user = await users.get(member.userId);
           return {
             ...member,
             name: user.name,
