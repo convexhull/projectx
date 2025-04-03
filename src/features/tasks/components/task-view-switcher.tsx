9fd9e4dceea3d3380768e1ff1194ee614c21aee0
Yash Pratap Singh <singhyashpratap06@gmail.com>
2025-04-03 16:35:29 +0530

    Creat api for bulk update of tasks for drag and drop and connect to kanban board, finish Kanban view

--- src/features/tasks/components/task-view-switcher.tsx
+++ src/features/tasks/components/task-view-switcher.tsx
@@ -11,6 +11,10 @@ import { DataFilters } from "./data-filters";
 import { useTaskFilters } from "../hooks/use-task-filters";
 import { DataTable } from "./data-table";
 import { columns } from "./columns";
+import { DataKanban } from "./data-kanban";
+import { useCallback } from "react";
+import { TaskStatus } from "../types";
+import { useBulkUpdateTask } from "../api/use-bulk-update-task";
 
 export const TaskViewSwitcher = () => {
   const [{ status, assigneeId, projectId, dueDate }] = useTaskFilters();
@@ -26,6 +30,16 @@ export const TaskViewSwitcher = () => {
     status,
   });
   const { open } = useCreateTaskModal();
+  const { mutate: bulkUpdate } = useBulkUpdateTask();
+
+  const onKanbanChange = useCallback(
+    (tasks: { $id: string; status: TaskStatus; position: number }[]) => {
+      bulkUpdate({
+        json: { tasks },
+      });
+    },
+    [bulkUpdate]
+  );
 
   return (
     <Tabs
@@ -64,7 +78,10 @@ export const TaskViewSwitcher = () => {
               {<DataTable columns={columns} data={tasks?.documents ?? []} />}
             </TabsContent>
             <TabsContent value="kanban" className="mt-0">
-              Data kanban
+              <DataKanban
+                onChange={onKanbanChange}
+                data={tasks?.documents ?? []}
+              />
             </TabsContent>
             <TabsContent value="calendar" className="mt-0">
               Data calendar
