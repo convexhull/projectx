d6332285d4ed7e4838d741b8785e45d52c7fcbb8
Yash Pratap Singh <singhyashpratap06@gmail.com>
2025-04-04 12:16:11 +0530

    Create My Tasks page, add conditional rendering to project filter in <DataFilters />

--- src/features/tasks/components/data-filters.tsx
+++ src/features/tasks/components/data-filters.tsx
@@ -17,7 +17,8 @@ import { DatePicker } from "@/components/date-picker";
 type DataFiltersProps = {
   hideProjectFilters?: boolean;
 };
-export const DataFilters = ({}: DataFiltersProps) => {
+
+export const DataFilters = ({ hideProjectFilters }: DataFiltersProps) => {
   const workspaceId = useWorkspaceId();
   const { data: projects, isLoading: isLoadingProjects } = useGetProjects({
     workspaceId,
@@ -103,28 +104,30 @@ export const DataFilters = ({}: DataFiltersProps) => {
           ))}
         </SelectContent>
       </Select>
-      <Select
-        defaultValue={projectId ?? undefined}
-        onValueChange={(value) => {
-          onProjectChange(value);
-        }}
-      >
-        <SelectTrigger className="w-full lg:w-auto h-8">
-          <div className="flex items-center pr-2">
-            <FolderIcon className="size-4 mr-2" />
-            <SelectValue placeholder="All projects" />
-          </div>
-        </SelectTrigger>
-        <SelectContent>
-          <SelectItem value="all">All projects</SelectItem>
-          <SelectSeparator />
-          {projectOptions?.map((project) => (
-            <SelectItem key={project.value} value={project.value}>
-              {project.label}
-            </SelectItem>
-          ))}
-        </SelectContent>
-      </Select>
+      {!hideProjectFilters && (
+        <Select
+          defaultValue={projectId ?? undefined}
+          onValueChange={(value) => {
+            onProjectChange(value);
+          }}
+        >
+          <SelectTrigger className="w-full lg:w-auto h-8">
+            <div className="flex items-center pr-2">
+              <FolderIcon className="size-4 mr-2" />
+              <SelectValue placeholder="All projects" />
+            </div>
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="all">All projects</SelectItem>
+            <SelectSeparator />
+            {projectOptions?.map((project) => (
+              <SelectItem key={project.value} value={project.value}>
+                {project.label}
+              </SelectItem>
+            ))}
+          </SelectContent>
+        </Select>
+      )}
       <DatePicker
         placeholder="Due date"
         className="h-12 w-full lg:w-auto"
