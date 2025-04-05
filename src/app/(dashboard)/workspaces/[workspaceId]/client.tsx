f0725f64d1048a778c3800f402986c8576d9f799
Yash Pratap Singh <singhyashpratap06@gmail.com>
2025-04-05 18:09:55 +0530

    Add members list to workspace analytics page, fix list key errors on this page

--- src/app/(dashboard)/workspaces/[workspaceId]/client.tsx
+++ src/app/(dashboard)/workspaces/[workspaceId]/client.tsx
@@ -7,8 +7,8 @@ import { PageLoader } from "@/components/page-loader";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 import { useGetMembers } from "@/features/members/api/use-get-members";
-// import { MemberAvatar } from "@/features/members/components/members-avatar";
-// import { Member } from "@/features/members/types";
+import { MemberAvatar } from "@/features/members/components/members-avatar";
+import { Member } from "@/features/members/types";
 import { useGetProjects } from "@/features/projects/api/use-get-projects";
 import { ProjectAvatar } from "@/features/projects/components/project-avatar";
 import { useCreateProjectModal } from "@/features/projects/hooks/use-create-project-modal";
@@ -19,7 +19,7 @@ import { Task } from "@/features/tasks/types";
 import { useGetWorkspaceAnalytics } from "@/features/workspaces/api/use-get-workspace-analytics";
 import { useWorkspaceId } from "@/features/workspaces/hooks/use-workspace-id";
 import { formatDistanceToNow } from "date-fns";
-import { CalendarIcon, PlusIcon } from "lucide-react";
+import { CalendarIcon, PlusIcon, SettingsIcon } from "lucide-react";
 import Link from "next/link";
 
 export const WorkspaceIdClient = () => {
@@ -57,7 +57,7 @@ export const WorkspaceIdClient = () => {
       <div className="grid grid-cols-1 xl:grid-cols-2 gap-4">
         <TaskList data={tasks.documents} total={tasks.total} />
         <ProjectList data={projects.documents} total={projects.total} />
-        {/* <MembersList data={members.documents} total={members.total} /> */}
+        <MembersList data={members.documents} total={members.total} />
       </div>
     </div>
   );
@@ -84,7 +84,7 @@ export const TaskList = ({ data, total }: TaskListProps) => {
         <DottedSeparator className="my-4" />
         <ul className="flex flex-col gap-y-4">
           {data.map((task) => (
-            <li key={task.id}>
+            <li key={task.$id}>
               <Link href={`/workspaces/${workspaceId}/tasks/${task.$id}`}>
                 <Card className="shadow-none rounded-lg hover:opacity-75 transition">
                   <CardContent className="p-4">
@@ -137,7 +137,7 @@ export const ProjectList = ({ data, total }: ProjectListProps) => {
         <DottedSeparator className="my-4" />
         <ul className="grid grid-cols-1 lg:grid-cols-2 gap-4">
           {data.map((project) => (
-            <li key={project.id}>
+            <li key={project.$id}>
               <Link href={`/workspaces/${workspaceId}/projects/${project.$id}`}>
                 <Card className="shadow-none rounded-lg hover:opacity-75 transition">
                   <CardContent className="p-4 flex items-center gap-x-2.5">
@@ -157,6 +157,53 @@ export const ProjectList = ({ data, total }: ProjectListProps) => {
           ))}
           <li className="text-sm text-muted-foreground text-center hidden first-of-type:block">
             No projects found
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+type MembersListProps = {
+  data: Member[];
+  total: number;
+};
+
+export const MembersList = ({ data, total }: MembersListProps) => {
+  const workspaceId = useWorkspaceId();
+
+  return (
+    <div className="flex flex-col gap-y-4 col-span-1">
+      <div className="bg-white border rounded-lg p-4">
+        <div className="flex items-center justify-between">
+          <p>Members ({total})</p>
+          <Button variant="secondary" size="icon" asChild>
+            <Link href={`/workspaces/${workspaceId}/members`}>
+              <SettingsIcon className="size-4 text-neutral-400" />
+            </Link>
+          </Button>
+        </div>
+        <DottedSeparator className="my-4" />
+        <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+          {data.map((member) => (
+            <li key={member.$id}>
+              <Card className="shadow-none rounded-lg overflow-hidden">
+                <CardContent className="p-3 flex flex-col items-center gap-x-2">
+                  <MemberAvatar name={member.name} className="size-12" />
+                  <div className="flex flex-col items-center overflow-hidden">
+                    <p className="text-lg font-medium line-clamp-1">
+                      {member.name}
+                    </p>
+                    <p className="text-sm text-muted-foreground line-clamp-1">
+                      {member.email}
+                    </p>
+                  </div>
+                </CardContent>
+              </Card>
+            </li>
+          ))}
+          <li className="text-sm text-muted-foreground text-center hidden first-of-type:block">
+            No members found
           </li>
         </ul>
       </div>
